74477ac68d69bf2dff6dd31ff0056e8e0f14e444
Deku <vuminhnhat1603@gmail.com>
2023-04-29 22:09:15 +0700

    search

--- controller/searchController.js
+++ controller/searchController.js
@@ -1,28 +1,27 @@
 const category= require("../model/categoryModel");
 const product = require("../model/productModel");
 const brand = require("../model/brandModel");
-const authors = require("../model/authorsModel");
+const author = require("../model/authorsModel");
 
 const  productController = {
     searchProduct:async (req, res)=>{
         try {
-            //const products = await product.find({ name : /Kiếp/});
             const search = req.query.search;            
             const products = await product.find(
                 { $text:{ $search: '\"' + search +'\"'} },
                 {score: {$meta: 'textScore'}}
             ).populate("category").populate("brand").populate("author");
-            const author =await authors.find(
-                { $text:{ $search: '\"' + search +'\"'} },
-                {score: {$meta: 'textScore'}}
-            )
-            for( let i =0; i< author.length; i++){
-                var productA= author[i].product;
-                for(let j =0; j< productA.length;j++){
-                    const aProduct =await product.findById(productA[j]).populate("category").populate("brand").populate("author");
-                    products.push(aProduct);
-                }
-            }
+            // const authors =await authors.find(
+            //     { $text:{ $search: '\"Brian L Weiss\"'} },
+            //     {score: {$meta: 'textScore'}}
+            // )
+            // for( let i =0; i< author.length; i++){
+            //     var productA= author[i].product;
+            //     for(let j =0; j< productA.length;j++){
+            //         const aProduct =await product.findById(productA[j]).populate("category").populate("brand").populate("author");
+            //         products.push(aProduct);
+            //     }
+            // }
             res.status(200).json(products);
         } catch (error) {
             res.status(500).json(error);
