d1a981d67e225f4b9f6634919b374a0bc7b16b43
DekuHxH <vuminhnhat1603@gmail.com>
2023-05-09 22:00:02 +0700

    search

--- controller/searchController.js
+++ controller/searchController.js
@@ -2,7 +2,7 @@ const category= require("../model/categoryModel");
 const product = require("../model/productModel");
 const brand = require("../model/brandModel");
 const author = require("../model/authorsModel");
-
+const _ = require("lodash");
 const  productController = {
     searchProduct:async (req, res)=>{
         try {
@@ -11,22 +11,44 @@ const  productController = {
                 { $text:{ $search: '\"' + search +'\"'} },
                 {score: {$meta: 'textScore'}}
             ).populate("category").populate("brand").populate("author");
-            // const authors =await authors.find(
-            //     { $text:{ $search: '\"Brian L Weiss\"'} },
-            //     {score: {$meta: 'textScore'}}
-            // )
-            // for( let i =0; i< author.length; i++){
-            //     var productA= author[i].product;
-            //     for(let j =0; j< productA.length;j++){
-            //         const aProduct =await product.findById(productA[j]).populate("category").populate("brand").populate("author");
-            //         products.push(aProduct);
-            //     }
-            // }
             res.status(200).json(products);
         } catch (error) {
             res.status(500).json(error);
         }
 
     },
+    searchByName: async (req, res)=>{
+        try {
+            const search = req.query.search;
+            const products = await product.find().populate("category").populate("brand").populate("author");
+            const result = products.filter((product) =>
+                _.toLower(_.deburr(product.name)).includes(_.toLower(_.deburr(search))));
+            res.status(200).json(result);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+    searchByDescription: async (req, res)=>{
+        try {
+            const search = req.query.search;
+            const products = await product.find().populate("category").populate("brand").populate("author");
+            const result = products.filter((product) =>
+                _.toLower(_.deburr(product.description)).includes(_.toLower(_.deburr(search))));
+            res.status(200).json(result);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+    searchByAuthor: async (req, res)=>{
+        try {
+            const search = req.query.search;
+            const products = await product.find().populate("category").populate("brand").populate("author");
+            const result = products.filter((product) =>
+                _.toLower(_.deburr(product.author.name)).includes(_.toLower(_.deburr(search))));
+            res.status(200).json(result);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
 }
 module.exports =productController;
--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -6,4 +6,10 @@ const router =require("express").Router();
 //SEARCH
 router.get("/", searchController.searchProduct);
 
+router.get("/name", searchController.searchByName);
+
+router.get("/description", searchController.searchByDescription);
+
+router.get("/author", searchController.searchByAuthor);
+
 module.exports =router;
