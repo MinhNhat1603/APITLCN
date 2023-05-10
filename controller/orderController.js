bdd0b281abb8f9353517169e5fd5fd2b4991dfd7
DekuHxH <vuminhnhat1603@gmail.com>
2023-05-10 20:37:55 +0700

    updateOrder

--- controller/orderController.js
+++ controller/orderController.js
@@ -57,7 +57,7 @@ const orderController = {
     updateOrder: async (req, res)=>{
         try {
             const Order =await order.findById(req.params.id);
-            await Order.updateOne({$set: req.query});
+            await Order.updateOne({$set: req.body});
             res.status(200).json("Update successfully!");
         } catch (error) {
             res.status(500).json(error);
