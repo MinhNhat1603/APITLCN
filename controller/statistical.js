a1be06d74e11aad5d87558d796ee45c07355fc3e
DekuHxH <vuminhnhat1603@gmail.com>
2023-05-11 16:55:20 +0700

    updateOrder

--- controller/statistical.js
+++ controller/statistical.js
@@ -14,7 +14,7 @@ const  statiscal ={
                       $lte: weeks[i].endDate
                   },
                    status:{
-                       $gte: 2
+                       $in: 2
                   }
                 });
                 //orders.push(order);
@@ -50,7 +50,7 @@ const  statiscal ={
                       $lte: months[i].endDate
                   },
                    status:{
-                       $gte: 2
+                       $in: 2
                   }
                 });
                 const report = {
